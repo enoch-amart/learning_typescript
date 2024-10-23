282146e7472194a11a9b3646af37f12e4bafdb2f
enoch-amart <55097378+enoch-amart@users.noreply.github.com>
2024-10-23 18:44:09 +0100

    Parameter Properties

--- src/classes.ts
+++ src/classes.ts
@@ -1,35 +1,35 @@
-class Account {
-  readonly id: number;
-  owner: string;
-  private _balance: number;
-  nickname?: string;
+// class Account {
+//   readonly id: number;
+//   owner: string;
+//   private _balance: number;
+//   nickname?: string;
 
-  constructor(id: number, owner: string, balance: number) {
-    this.id = id;
-    this.owner = owner;
-    this._balance = balance;
-  }
+//   constructor(id: number, owner: string, balance: number) {
+//     this.id = id;
+//     this.owner = owner;
+//     this._balance = balance;
+//   }
 
-  private calculateTax(): number {
-    return 0;
-  }
+//   private calculateTax(): number {
+//     return 0;
+//   }
 
-  getBalance(): number {
-    return this._balance;
-  }
+//   getBalance(): number {
+//     return this._balance;
+//   }
 
-  deposit(amount: number): void {
-    if (amount <= 0) throw new Error("Invalid amount");
-    this._balance += amount;
-  }
+//   deposit(amount: number): void {
+//     if (amount <= 0) throw new Error("Invalid amount");
+//     this._balance += amount;
+//   }
 
-  withdraw(amount: number): void {
-    if (amount <= 0) throw new Error("Invalid amount");
-    this._balance -= amount;
-  }
-}
+//   withdraw(amount: number): void {
+//     if (amount <= 0) throw new Error("Invalid amount");
+//     this._balance -= amount;
+//   }
+// }
 
-let account = new Account(1, "Enoch", 0);
+// let account = new Account(1, "Enoch", 0);
 // account.deposit(100);
 // console.log(account.balance);
 // account.withdraw(50);
@@ -37,4 +37,20 @@ let account = new Account(1, "Enoch", 0);
 
 // console.log(account instanceof Account);
 
-console.log(account.getBalance());
+// console.log(account.getBalance());
+
+// Parameter properties
+class Account {
+  nickname?: string;
+
+  constructor(
+    public readonly id: number,
+    public owner: string,
+    private _balance: number
+  ) {}
+
+  deposit(amount: number): void {
+    if (amount <= 0) throw new Error("Invalid amount");
+    this._balance += amount;
+  }
+}
