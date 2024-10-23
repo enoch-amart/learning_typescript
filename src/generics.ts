9caa2c099e06d7115cc93fd58f96d142ff170f27
enoch-amart <55097378+enoch-amart@users.noreply.github.com>
2024-10-23 22:19:21 +0100

    generic interfaces

--- src/generics.ts
+++ src/generics.ts
@@ -8,11 +8,36 @@
 // let newPair = new KeyValuePair("hola", "great");
 
 // Generic Functions
-class ArrayUtils {
-  wrapInArray<T>(value: T) {
-    return [value];
-  }
+// class ArrayUtils {
+//   wrapInArray<T>(value: T) {
+//     return [value];
+//   }
+// }
+
+// let arrays = new ArrayUtils();
+// let numbers = arrays.wrapInArray(1);
+
+// Generic interfaces
+
+interface Result<T> {
+  data: T | null;
+  error: string | null;
 }
 
-let arrays = new ArrayUtils();
-let numbers = arrays.wrapInArray(1);
+function fetch<T>(url: string): Result<T> {
+  return { data: null, error: null };
+}
+
+interface User {
+  username: string;
+}
+
+interface Product {
+  title: string;
+}
+
+let result = fetch<User>("url");
+result.data?.username;
+
+let result1 = fetch<Product>("url");
+result1.data?.title;
