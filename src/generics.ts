e66fbd8571f67f8911175d5d829ded1d2ab32730
enoch-amart <55097378+enoch-amart@users.noreply.github.com>
2024-10-23 23:15:47 +0100

    generic contraints

--- src/generics.ts
+++ src/generics.ts
@@ -19,25 +19,60 @@
 
 // Generic interfaces
 
-interface Result<T> {
-  data: T | null;
-  error: string | null;
+// interface Result<T> {
+//   data: T | null;
+//   error: string | null;
+// }
+
+// function fetch<T>(url: string): Result<T> {
+//   return { data: null, error: null };
+// }
+
+// interface User {
+//   username: string;
+// }
+
+// interface Product {
+//   title: string;
+// }
+
+// let result = fetch<User>("url");
+// result.data?.username;
+
+// let result1 = fetch<Product>("url");
+// result1.data?.title;
+
+// Generic constraints
+interface Person {
+  name: string;
 }
 
-function fetch<T>(url: string): Result<T> {
-  return { data: null, error: null };
+class Persons {
+  constructor(public name: string) {}
 }
 
-interface User {
-  username: string;
+class customer extends Persons {}
+
+// type contraint
+function echo<T extends string | number>(value: T): T {
+  return value;
+}
+echo("hello");
+
+// Shape object contraint
+function echo2<T extends { name: string }>(value: T): T {
+  return value;
+}
+echo2({ name: "enoch" });
+
+// interface contraint
+function echo3<T extends Person>(value: T): T {
+  return value;
 }
 
-interface Product {
-  title: string;
+// class contraint
+function echo4<T extends Person>(value: T): T {
+  return value;
 }
-
-let result = fetch<User>("url");
-result.data?.username;
-
-let result1 = fetch<Product>("url");
-result1.data?.title;
+echo4(new customer("a"));
+echo4(new Persons("hello"));
