f564818716b275e66dc5249c2508645cf7bd0963
enoch-amart <55097378+enoch-amart@users.noreply.github.com>
2024-10-23 20:41:02 +0100

    Exercises for Classes and Interfaces

--- src/exercises.ts
+++ src/exercises.ts
@@ -46,3 +46,45 @@
 // let x = foo ?? bar();
 
 // // Exercise 5
+
+// Exercises - Classes, Interfaces and Object-oriented Programming
+// exercise 1
+class Logger {
+  constructor(public fileName: string) {}
+
+  write(message: string): string {
+    return "hello";
+  }
+}
+
+// Exercise 2
+class Person {
+  constructor(public firstName: string, public lastName: string) {}
+
+  get fullName() {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+// Exercise 3
+class Employee extends Person {
+  constructor(public salary: number, firstName: string, lastName: string) {
+    super(firstName, lastName);
+  }
+}
+
+// exercise 4
+// Private members are not inherited by child classes
+
+// exercise 5
+interface Address {
+  street: string;
+  city: string;
+  zipCode: number;
+}
+
+interface Employees {
+  name: string;
+  salary: number;
+  address: Address;
+}
