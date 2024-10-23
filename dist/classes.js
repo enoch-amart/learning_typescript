0bfd2329911c980c23ca6e73edccd17e067da7d7
enoch-amart <55097378+enoch-amart@users.noreply.github.com>
2024-10-23 18:17:09 +0100

    starting with classess

---
+++ dist/classes.js
@@ -0,0 +1,19 @@
+"use strict";
+class Account {
+    constructor(id, owner, balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+    deposit(amount) {
+        if (amount <= 0)
+            throw new Error("Invalid amount");
+        this.balance += amount;
+    }
+    withdraw(amount) {
+        if (amount <= 0)
+            throw new Error("Invalid amount");
+        this.balance -= amount;
+    }
+}
+//# sourceMappingURL=classes.js.map
