a6c43b7660a05abc31d75479478a82af0e813fc5
enoch-amart <55097378+enoch-amart@users.noreply.github.com>
2024-10-23 13:32:26 +0100

    getting to know funcitons in typescript

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,19 @@
 // const large = 3;
 
 // using enum to declare them. use PascalCase naming
-const enum Size {
-  Small = 1,
-  Medium,
-  Large,
+// const enum Size {
+//   Small = 1,
+//   Medium,
+//   Large,
+// }
+
+// let mySize: Size = Size.Medium;
+// console.log(mySize);
+
+// Functions - ? to make the parameter optional eg.  taxYear?: number
+function calculateTax(income: number, taxYear = 2022): number {
+  if (taxYear < 2022) return income * 1.2;
+  return income * 1.3;
 }
 
-let mySize: Size = Size.Medium;
-console.log(mySize);
+calculateTax(10_000);
