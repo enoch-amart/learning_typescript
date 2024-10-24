c5e83bdc86345e424f9b8cd5a0173721a0149f55
enoch-amart <55097378+enoch-amart@users.noreply.github.com>
2024-10-24 17:25:59 +0100

    Accessor Decorators

--- src/decorators.ts
+++ src/decorators.ts
@@ -26,31 +26,57 @@
 // class ProfileComponent {}
 
 // Method Decorations
-function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
-  const original = descriptor.value as Function;
-  descriptor.value = function (message: string) {
-    console.log("Before");
-    original.call(this, message);
-    console.log("After");
+// function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
+//   const original = descriptor.value as Function;
+//   descriptor.value = function (message: string) {
+//     console.log("Before");
+//     original.call(this, message);
+//     console.log("After");
+//   };
+// }
+
+// // making it more generic.
+// function Log1(target: any, methodName: string, descriptor: PropertyDescriptor) {
+//   const original = descriptor.value as Function;
+//   descriptor.value = function (...args: any) {
+//     console.log("Before");
+//     original.call(this, ...args);
+//     console.log("After");
+//   };
+// }
+
+// class Person {
+//   @Log
+//   say(message: string) {
+//     console.log("Person says " + message);
+//   }
+// }
+
+// let person = new Person();
+// person.say("Hello world");
+
+// Accessor Decorators
+
+function Capitalize(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const original = descriptor.get;
+  descriptor.get = function () {
+    const result = original?.call(this);
+    return typeof result === "string" ? result.toUpperCase() : result;
   };
 }
-
-// making it more generic.
-function Log1(target: any, methodName: string, descriptor: PropertyDescriptor) {
-  const original = descriptor.value as Function;
-  descriptor.value = function (...args: any) {
-    console.log("Before");
-    original.call(this, ...args);
-    console.log("After");
-  };
-}
-
 class Person {
-  @Log
-  say(message: string) {
-    console.log("Person says " + message);
+  constructor(public firstName: string, public lastName: string) {}
+
+  @Capitalize
+  get fullName() {
+    // return null;
+    return `${this.firstName} ${this.lastName}`;
   }
 }
 
-let person = new Person();
-person.say("Hello world");
+let person = new Person("enoch", "amarteifio");
+console.log(person.fullName);
