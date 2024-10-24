88d9f1e40b173ed7335351c3d2b37cea9cf4f501
enoch-amart <55097378+enoch-amart@users.noreply.github.com>
2024-10-24 16:34:00 +0100

    parameterized decorators

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,10 +1,19 @@
-function Component(constructor: Function) {
-  console.log("Component decorator called");
-  constructor.prototype.uniqueId = Date.now();
-  constructor.prototype.insertInDOM = () => {
-    console.log("Inserting the component in the DOM");
+// parameterized decorators
+type ComponentOptions = {
+  selector: string;
+};
+
+// decorator Factory
+function Component(value: ComponentOptions) {
+  return (constructor: Function) => {
+    console.log("Component decorator called");
+    constructor.prototype.options = value;
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDOM = () => {
+      console.log("Inserting the component in the DOM");
+    };
   };
 }
 
-@Component
+@Component({ selector: "#my-profile" })
 class ProfileComponent {}
